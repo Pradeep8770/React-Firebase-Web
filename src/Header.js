b4cf7ba9e81714ae5c3c0a6949c88f38ba145447
Pradeep8770 <pradeeppatel5148@gmail.com>
2023-04-12 18:00:17 +0530

    created responsiv navbar

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <section className="container main-hero-container">
+          <div className="row">
+            <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start">
+              <h1 className="display2">
+                online payment Made <br />
+                Easy for you.
+              </h1>
+              <p className="main-hero-para">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in
+              </p>
+              <h3>get early access for you</h3>
+              <div className="input-group mt-3">
+                <input
+                  type="text"
+                  className="rounded-pill w-75 me-3 p-2 form-control-plaintext"
+                />
+                <div className="input-group-text">Get it now</div>
+              </div>
+            </div>
+            {/* {------------------main heade right side----------------} */}
+            <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center">
+              <img src="./Images/hero1.jpg" alt="heroimg" className="img-fluid"/>
+              <img src="./Images/hero2.jpg" alt="heroimg" className="img-fluid"/>
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
+}
